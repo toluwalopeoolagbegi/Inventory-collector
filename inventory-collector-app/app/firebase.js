733b257d82c16e68ae9fd337e33ef14c8ef12c6a
Toluwalope Olateru-Olagbegi <wole2003@gmail.com>
2024-09-03 22:55:20 -0500

    chore

--- inventory-collector-app/app/firebase.js
+++ inventory-collector-app/app/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyC9zsGQnZuDyPANhI_SZsN5aOqUqCbU68w",
+    authDomain: "inventory-collector-478dd.firebaseapp.com",
+    projectId: "inventory-collector-478dd",
+    storageBucket: "inventory-collector-478dd.appspot.com",
+    messagingSenderId: "332068158668",
+    appId: "1:332068158668:web:de75976db1e833d8161669"
 };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
